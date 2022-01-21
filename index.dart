579e6fd0226016fb3c5e998a291cae53d32b7397
OlaideOlapade <Emiloju@stitch.org>
2022-01-21 15:03:20 +0100

    add form submission

---
+++ index.dart
@@ -0,0 +1,53 @@
+
+void main(){
+  print("Hello World");
+
+  //statement
+  print("This is a statement"); //command given to computer to work on and it ends with a semicolon (;)
+
+  //expression
+  1;
+  2-3;
+
+  //***variable***
+
+  //var name3 = "Olaide";
+  //num = 2456 or 0.3
+  //String = "iutgyjfdcfsg"
+  //int = 53535345356
+  //double = 0.345
+  //list = [[12], 3, "jhghvdg", 0.3, ]
+  //map map = {name: "Ola", age:23}
+  //set name = {1,2,3,4,5,6,7,8}
+  //dynamic can accept any data type value
+  //final
+  //const
+
+
+  //****Rules of naming variables****
+  //cannot use the same name
+  //cannot start with number
+  //cannot use spaces
+  //cannot use keyword
+
+
+
+  //****Naming Convention****
+  //pascal naming = MyAge
+  //camelCasing = myAge i.e starting with a small letter and the word that follows start with a capital
+  //snake = my_age i.e using underscore sign 
+
+
+  //***interpolation*** = the representation of dynamic value in a print function or string
+  var age = 4;
+  var nextAge = 3;
+  print("My age is ${age + nextAge}");
+
+  //input
+  //var age1 = stdin.readLineSync();
+
+
+
+  //***Assignment***
+  //build a programme that will ask for User Age, Name, School Name. and print it
+}
